98a0f61cf9628b5ff8993c1ee36cdf53fee41876
Joanna Czepiec <joanna.czepiec7@gmail.com>
2017-05-09 13:24:21 +0200

    Integration of calculateAPpositions function

--- hew-outdoor.cc
+++ hew-outdoor.cc
@@ -24,6 +24,11 @@
 #include "ns3/mobility-module.h"
 #include "ns3/internet-module.h"
 
+#include<iostream>
+#include<vector>
+#include<math.h>
+#include<string>
+#include <fstream>
 
 // This is an implementation of the TGax (HEW) outdoor scenario.
 
@@ -32,150 +37,43 @@ using namespace ns3;
 NS_LOG_COMPONENT_DEFINE ("hew-outdoor");
 
 int countAPs(int layers); // Count the number of APs per layer
+double **calculate_AP_positions(int h, int layers); //Calculate the positions of AP
+
 
 int main (int argc, char *argv[])
 {
 
 	/* Initialize parameters */
 	double simulationTime = 10; //seconds
-    int layers=3; 
-    bool debug=false;
+  int layers=3;
+  bool debug=false;
+	int h=65; //distance between AP/2
 
 	/* Command line parameters */
 
 	CommandLine cmd;
 	cmd.AddValue ("simulationTime", "Simulation time [s]", simulationTime);
-    cmd.AddValue ("layers", "Number of layers in hex grid", layers);
-    cmd.AddValue ("debug", "Enable debug mode", debug);
+  cmd.AddValue ("layers", "Number of layers in hex grid", layers);
+  cmd.AddValue ("debug", "Enable debug mode", debug);
 	cmd.Parse (argc,argv);
 
 	/* Position APs */
     if(debug){
         std::cout << "There are "<< countAPs(layers) << " APs in " << layers << " layers.\n";
     }
-  
-	
-/*
+
+
+
 // calculate_AP_position function
-	double **calculate_AP_positions(double first_x, double first_y, int h, int layers){
+	double ** APpositions;
+	APpositions = calculate_AP_positions(h, layers);
 
-    float sq=sqrt(3);
-
-    float x=0;
-    float y=0;
-
-    int dimension=2; //number of dimensions: x and y
-    int APnum=AP_number(layers); //number of AP calculated by the AP_number function
-
-    cout<<APnum<<endl;
-
-
-    vector <float> x_co;
-    vector <float> y_co;
-
-    x_co.push_back(first_x);
-    y_co.push_back(first_y);
-
-        for(int lay=1; lay<layers;lay++){
-            for(int k=1;k<=7;k++){
-                if(k==1){
-                        x=x+h*sq;
-                        y=y+h;
-
-                        x_co.push_back(x);
-                        y_co.push_back(y);
-
-                    }
-                else if(k==2){
-                    for(int hex_lay=1; hex_lay<=lay; hex_lay++){
-
-                        x=x-h*sq;
-                        y=y+h;
-
-                        x_co.push_back(x);
-                        y_co.push_back(y);
-                    }
-
-                    }
-
-                else if(k==3){
-                    for(int hex_lay=1; hex_lay<=lay; hex_lay++){
-
-                        x=x-h*sq;
-                        y=y-h;
-
-                        x_co.push_back(x);
-                        y_co.push_back(y);
-                    }
-                    }
-                else if(k==4){
-                    for(int hex_lay=1; hex_lay<=lay; hex_lay++){
-
-                        y=y-2*h;
-
-                        x_co.push_back(x);
-                        y_co.push_back(y);
-                    }
-                    }
-                else if(k==5){
-                    for(int hex_lay=1; hex_lay<=lay; hex_lay++){
-
-                        x=x+h*sq;
-                        y=y-h;
-
-                        x_co.push_back(x);
-                        y_co.push_back(y);
-                    }
-                    }
-                else if(k==6){
-                    for(int hex_lay=1; hex_lay<=lay; hex_lay++){
-
-                        x=x+h*sq;
-                        y=y+h;
-
-                        x_co.push_back(x);
-                        y_co.push_back(y);
-                    }
-                    }
-                else if(k==7){
-                        for(int hex_lay=1; hex_lay<=lay; hex_lay++){
-                            y=y+2*h;
-                            if(hex_lay<lay){
-                                x_co.push_back(x);
-                                y_co.push_back(y);
-                            }
-                }
-                }
-            }
-            }
-
-
-    string file_name="coord.ods";
-
-    std::ofstream coordfile;
-    coordfile.open (file_name.c_str(), ios::app);
-
-
-
- double** AP_co=0;
-    AP_co = new double*[2];
-    AP_co[0]=new double[APnum];
-    AP_co[1]=new double[APnum];
-
-    for (int p=0;p<APnum;p++){
-        AP_co[0][p]=x_co[p];
-        AP_co[1][p]=y_co[p];
-    }
-
-
-      for ( int m = 0; m < APnum; ++m){
-      coordfile<<x_co[m]<<"\t"<<y_co[m]<<endl;
-      }
-
-    coordfile.close();
-    return AP_co;
-    }
-*/
+	//how it works
+	if(debug){
+		for (int m=0; m<countAPs(layers);m++){
+			std::cout << APpositions[0][m]<< "\t" <<APpositions[1][m]<<std::endl;
+		}
+	}
 	//(x, y) = calculateAPpositions(nAP);
 	//placeAP(x, y, NodeContainer);
 
@@ -210,7 +108,7 @@ int main (int argc, char *argv[])
         return 0;
 }
 
-int countAPs(int layers){ 
+int countAPs(int layers){
     int APsum=1; //if 1 layer then 1 AP
     if(layers>1) {
         for(int i=0; i<layers; i++) {
@@ -259,3 +157,122 @@ void showPosition(NodeContainer &accessPoint)
 
 }
 */
+
+double **calculate_AP_positions(int h, int layers){
+
+	float sq=sqrt(3);
+  float first_x=0; // coordinates of the first AP
+	float first_y=0;
+	float x=0;
+	float y=0;
+
+
+	int APnum=countAPs(layers); //number of AP calculated by the AP_number function
+
+	std::cout<<APnum<<std::endl;
+
+
+	std::vector <float> x_co;
+	std::vector <float> y_co;
+
+	x_co.push_back(first_x);
+	y_co.push_back(first_y);
+
+			for(int lay=1; lay<layers;lay++){
+					for(int k=1;k<=7;k++){
+							if(k==1){
+											x=x+h*sq;
+											y=y+h;
+
+											x_co.push_back(x);
+											y_co.push_back(y);
+
+									}
+							else if(k==2){
+									for(int hex_lay=1; hex_lay<=lay; hex_lay++){
+
+											x=x-h*sq;
+											y=y+h;
+
+											x_co.push_back(x);
+											y_co.push_back(y);
+									}
+
+									}
+
+							else if(k==3){
+									for(int hex_lay=1; hex_lay<=lay; hex_lay++){
+
+											x=x-h*sq;
+											y=y-h;
+
+											x_co.push_back(x);
+											y_co.push_back(y);
+									}
+									}
+							else if(k==4){
+									for(int hex_lay=1; hex_lay<=lay; hex_lay++){
+
+											y=y-2*h;
+
+											x_co.push_back(x);
+											y_co.push_back(y);
+									}
+									}
+							else if(k==5){
+									for(int hex_lay=1; hex_lay<=lay; hex_lay++){
+
+											x=x+h*sq;
+											y=y-h;
+
+											x_co.push_back(x);
+											y_co.push_back(y);
+									}
+									}
+							else if(k==6){
+									for(int hex_lay=1; hex_lay<=lay; hex_lay++){
+
+											x=x+h*sq;
+											y=y+h;
+
+											x_co.push_back(x);
+											y_co.push_back(y);
+									}
+									}
+							else if(k==7){
+											for(int hex_lay=1; hex_lay<=lay; hex_lay++){
+													y=y+2*h;
+													if(hex_lay<lay){
+															x_co.push_back(x);
+															y_co.push_back(y);
+													}
+							}
+							}
+					}
+					}
+
+
+	//std::string file_name="coord.ods";
+	//std::ofstream coordfile;
+	//coordfile.open (file_name.c_str(), std::ios::app);
+
+
+
+double** AP_co=0;
+	AP_co = new double*[2];
+	AP_co[0]=new double[APnum];
+	AP_co[1]=new double[APnum];
+
+	for (int p=0;p<APnum;p++){
+			AP_co[0][p]=x_co[p];
+			AP_co[1][p]=y_co[p];
+	}
+
+
+	//	for ( int m = 0; m < APnum; ++m){
+	//	coordfile<<x_co[m]<<"\t"<<y_co[m]<<std::endl;
+	//	}
+
+	//coordfile.close();
+	return AP_co;
+	}
